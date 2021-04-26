4b409bfd24f241fa00951f4d7c8a995e2522f47b
SABBIR AHMED TALUKDAR <70398163+satsabbir11@users.noreply.github.com>
2021-04-26 11:04:17 +0600

    end of launcher project

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 function launcher_setup_theme()
 {
     load_theme_textdomain("launcher");
-    add_theme_support("post-thumbnails");
+    add_theme_support(" post-thumbnails ");
     add_theme_support("title-tag");
 }
 add_action("after_setup_theme","launcher_setup_theme");
